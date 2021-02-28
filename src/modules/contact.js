a970ffb90cd96f652fd2a57b2add68ccac98ac6e
Thiago <thiagoroberto2003@gmail.com>
2021-02-28 10:48:27 -0300

    Finished

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -24,9 +24,9 @@ function contact () {
     iframe.id = 'iframe';
 
     iframe.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.652224710078!2d-44.00136518510532!3d-19.939054943740068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6965ceade4c53%3A0x4980bb6236578f78!2sCEFET-MG%20-%20Campus%20Nova%20Gameleira!5e0!3m2!1spt-BR!2sbr!4v1613073521709!5m2!1spt-BR!2sbr";
-    teleImg.src = '/images/telefone-de-design-antigo.svg';
-    locationImg.src  = '/images/location..svg';
-    clockImg.src = '/images/clock-svgrepo-com.svg';
+    teleImg.src = 'images/telefone-de-design-antigo.svg';
+    locationImg.src  = 'images/location..svg';
+    clockImg.src = 'images/clock-svgrepo-com.svg';
     
     h2Tele.textContent = '(99) 0 1111-0000';
     h2Location.textContent = 'Av. Amazonas, 7675 - Nova Gameleira, Bh - MG';
